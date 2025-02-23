1c38fcc5a84f92de58ff8936ece6abe55baaef05
tharu-thennakoon <tharushikalpani2003@gmail.com>
2025-02-23 10:51:32 +0530

    Add Scroll to Top button and update styles

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ function activeLink() {
 }
 list.forEach((item) =>
 item.addEventListener('click',activeLink));
+
+// Scroll to Top Button
+const scrollToTopBtn = document.getElementById("scrollToTop");
+
+window.onscroll = function () {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        scrollToTopBtn.style.display = "flex";
+    } else {
+        scrollToTopBtn.style.display = "none";
+    }
+};
+
+scrollToTopBtn.onclick = function () {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+};
